ac922bfcf7ebbdfcb08c56eefefda42e87f29423
karimbaidori10 <karimbaidori10@gmail.com>
2026-06-02 01:51:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
 const { MongoClient, ObjectId } = require("mongodb");
 
